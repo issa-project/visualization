f7d4f32cfb91a95b0485aaea1aa8f11ac1a1b431
Youssef Mekouar <youssef.mekouar@etu.unice.fr>
2021-04-15 20:44:15 +0000

    Update component #6

--- src/Component/DataInfo.js
+++ src/Component/DataInfo.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const DataInfo = (props) => {
     const [popoverOpen, setPopoverOpen] = useState(false);
@@ -7,13 +8,13 @@ const DataInfo = (props) => {
     const toggle = () => setPopoverOpen(!popoverOpen);
 
     return (
-        <div>
+        <div className="entity">
             <Button id="Popover1" type="button">
-                Launch Popover
+                PCR
             </Button>
-            <Popover placement="bottom" isOpen={popoverOpen} target="Popover1" toggle={toggle}>
-                <PopoverHeader>Popover Title</PopoverHeader>
-                <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
+            <Popover placement="top" isOpen={popoverOpen} target="Popover1" toggle={toggle}>
+                <PopoverHeader> PCR </PopoverHeader>
+                <PopoverBody> in vitro method for producing large amounts of specific DNA or RNA fragments from small amounts of short oligonucleotide primers. </PopoverBody>
             </Popover>
         </div>
     );
