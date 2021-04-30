0962baaf4f7d1cfad9d55e22ffa46e3a2fd63936
Youssef Mekouar <youssef.mekouar@etu.unice.fr>
2021-04-30 09:01:57 +0200

    Add component NavBar #3

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import TextHighlight2 from './Component/TextHighlight2';
 import { Footer }  from './Component/Footer';
 import ArticleInfo from './Component/ArticleInfo';
+import NavBar from './Component/NavBar';
 
 
 function App() {
   return (
       <div>
+          <NavBar/>
           <ArticleInfo/>
           <TextHighlight2/>
             <Footer/>
---
+++ src/Component/NavBar.js
@@ -0,0 +1,29 @@
+import {Component} from "react";
+import { Navbar, Nav ,Button , Form , FormControl} from "react-bootstrap";
+import agrilogo from './Agritrop.png';
+
+
+class NavBar extends Component {
+
+    render() {
+        return(
+            <Navbar bg="light" variant="light">
+                <Navbar.Brand href="#home">
+                </Navbar.Brand>
+                <img src={agrilogo} alt="logo"></img>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Advanced Search</Nav.Link>
+                    <Nav.Link href="#features"> Cirad Authors</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-light">Search</Button>
+                </Form>
+            </Navbar>
+
+        );
+    }
+
+}
+export default NavBar;
+
