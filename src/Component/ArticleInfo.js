2344c65d4b72c22843e83f569eeee7896076f28d
Youssef Mekouar <youssef.mekouar@etu.unice.fr>
2021-05-07 13:49:59 +0200

    Update first Composant presentation

--- src/Component/ArticleInfo.js
+++ src/Component/ArticleInfo.js
@@ -1,40 +1,41 @@
 import React ,{ Component } from 'react';
 import './ArticleInfo.css';
-import { ListGroup } from 'react-bootstrap'
-//import backLogo from './logo-agritrop.png'
-
+import pdfDow from './pdf2.png';
 
 class ArticleInfo extends Component {
 
 
     render () {
         return (
-
             <div className="compoTitle">
                 <div className="Title">
-                    <h4>Title : A real-time PCR for SARS-coronavirus incorporating target gene pre-amplification</h4>
-                </div>
+                    <div>
+                    <p className="TitleText">A real-time PCR for SARS-coronavirus incorporating target gene pre-amplification
+                        <span className="date">2020</span>
+                        <span className="type">Academic Article and research paper </span>
+                        <span className="auteurs">Li Hui Wang,Chen Dillon ,Natalie Wong,Freda Pui-Fan Chan,Paul Cheung,Albert Fung </span>
+                        <span className="nbPage">116 pages.</span>
+                        <span className="idArticle">ISBN 978-2869-778388-9</span>
+                    </p>
+                    </div>
 
-                <div className="Type">
-                    Type : Academic Article and research paper
-                </div>
+                    <div>
+                        <p>
+                            Version publiée - <span className="nameVersion"> Français </span>
+                            Sous licence <span className="versionPDF">CC0 1.0</span>
+                            Sans restriction de droits pour le monde entier.
+                            <span className="namePDF"> Livre OCEAN FORESCOM VFinale 2020.pdf</span>
+                        </p>
+                        <img src={pdfDow} alt="pdf"/>
 
-                <div>
-                    <h4> Authors : </h4>
-                    <ListGroup variant="flush">
-                        <ListGroup.Item>  Li Hui Wang </ListGroup.Item>
-                        <ListGroup.Item> Chen Dillon </ListGroup.Item>
-                        <ListGroup.Item> Natalie Wong </ListGroup.Item>
-                        <ListGroup.Item> Freda Pui-Fan Chan </ListGroup.Item>
-                        <ListGroup.Item> Paul Cheung </ListGroup.Item>
-                        <ListGroup.Item> Albert Fung </ListGroup.Item>
-                    </ListGroup>
-                </div>
+                    </div>
 
+                </div>
 
 
 
             </div>
+
 
 
         );
