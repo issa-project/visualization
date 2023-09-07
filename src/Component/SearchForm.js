6470b432c119da7fc5d03c71ff65c8348e06c467
Franck Michel <frmichel@users.noreply.github.com>
2023-09-07 16:07:59 +0200

    Temp status

--- src/Component/SearchForm.js
+++ src/Component/SearchForm.js
@@ -1,68 +1,85 @@
-import React, { useState } from "react";
-import {Button} from "react-bootstrap";
+import React, {useState} from "react";
+import {Button, Form, Row, Col} from "react-bootstrap";
+import {ToggleButtonGroup, ToggleButton} from "react-bootstrap";
 
-function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
+const SearchForm = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [results, setResults] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
 
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearch = () => {
     // Call your API endpoint here with the search term and set the results
-    // You can replace the below code with your own API call
-    fetch(`https://jsonplaceholder.typicode.com/posts?q=${searchTerm}`)
-      .then((response) => response.json())
-      .then((data) => setResults(data));
-  };
+    const handleSearch = () => {
+        fetch(`https://jsonplaceholder.typicode.com/posts?q=${searchTerm}`)
+            .then((response) => {
+                    return response.json();
+            })
+            .then((data) => setResults(data));
+    };
 
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
+    const handleChange = (event) => {
+        console.log("Event : " + event.target.value);
+        setSearchTerm(event.target.value)
+    };
 
-  const pageSize = 10;
-  const pageCount = Math.ceil(results.length / pageSize);
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+            handleSearch();
+        }
+    };
 
-  const displayedResults = results.slice(
-    (currentPage - 1) * pageSize,
-    currentPage * pageSize
-  );
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
 
-  return (
-    <div>
-      <form>
-        <label>
-          Search:
-          <input type="text" value={searchTerm} onChange={handleChange} />
-        </label>
+    const pageSize = 10;
+    const pageCount = Math.ceil(results.length / pageSize);
 
-        <Button id="search" type="button" className="btn highlight-entity" onClick={handleSearch}>
-          Search
-        </Button>
+    const displayedResults = results.slice(
+        (currentPage - 1) * pageSize,
+        currentPage * pageSize
+    );
 
-      </form>
-      <ul>
-        {displayedResults.map((result) => (
-          <li key={result.id}>{result.title}</li>
-        ))}
-      </ul>
-      <div>
-        {Array.from({ length: pageCount }, (_, index) => index + 1).map(
-          (page) => (
-            <button
-              key={page}
-              type="button"
-              onClick={() => handlePageChange(page)}
-            >
-              {page}
-            </button>
-          )
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div className="component">
+
+            <Form>
+                <Row className="mb-3">
+                    <Col sm={8}>
+                        <Form.Control
+                            size="sm" type="text" placeholder="Search keywords" value={searchTerm}
+                            onChange={handleChange}
+                            onKeyPress={handleKeyPress}
+                        />
+                    </Col>
+                    <Col sm={2}>
+                        <Button id="search" size="sm" variant="secondary" type="button"
+                                onClick={handleSearch}>
+                            Search
+                        </Button>
+                    </Col>
+                </Row>
+            </Form>
+
+            <ul>
+                {displayedResults.map((result) => (
+                    <li key={result.id}>{result.title}</li>
+                ))}
+            </ul>
+
+            <div>
+                <ToggleButtonGroup size="sm" type="radio" name="pages" defaultValue={1}>
+                    {Array.from({length: pageCount}, (_, index) => index + 1).map(
+                        (page) => (
+                            <ToggleButton key={page} variant="light" type="button" value={page} onClick={() => handlePageChange(page)}>
+                                {page}
+                            </ToggleButton>
+                        )
+                    )}
+                </ToggleButtonGroup>
+            </div>
+
+        </div>
+    );
 }
 
-export default App;
+export default SearchForm;
--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,17 @@
 import './App.css';
+
+import {Container} from 'react-bootstrap';
 import SearchForm from './Component/SearchForm';
 import Footer from './Component/Footer';
 import NavBar from './Component/NavBar';
 
 function Search() {
     return (
-        <div className="container">
+        <Container fluid="xl">
             <NavBar/>
             <SearchForm/>
             <Footer/>
-        </div>
+        </Container>
     );
 }
 
